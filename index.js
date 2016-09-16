4162673afafbf828c71cc5f9a06c9e6648325c92
Ryan Kotzen <ryan@encentivize.co.za>
2016-09-16 08:43:40 +0200

    better error message if parsing the response fails.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 var WebSocket = require('ws');
 var ProtoBuf = require('protobufjs');
-
+var util = require('util');
+var os = require('os');
 var builder = ProtoBuf.loadProtoFile('./krpc.proto');
 var proto = builder.build();
 
@@ -15,9 +16,24 @@ sock.onopen = () => {
     sock.send(connectionRequest.toArrayBuffer());
 };
 
-sock.onmessage = (ev) => {
-    var buf = Buffer.from(ev.data);
-    //let resp = proto.krpc.schema.ConnectionResponse.decode(ev.data);
-    var msg = buf.toString('utf8');
-    console.log(msg);
-};
+sock.onmessage = messageReceived;
+
+function messageReceived(event) {
+    let resp;
+    try {
+        resp = proto.krpc.schema.Response.decode(event.data);
+    }
+    catch (err) {
+        var parsedMessage;
+        try {
+            var buf = Buffer.from(event.data);
+            parsedMessage = buf.toString('utf8');
+        }
+        catch (parsingError) {
+            parsedMessage = "Error parsing binary data :" + parsingError;
+        }
+        throw new Error(util.format(err.message, os.EOL, parsedMessage));
+
+    }
+    console.log(resp);
+}
