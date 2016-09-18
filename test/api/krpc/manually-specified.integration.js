8f5822be093d8219abc797ee93a4af8a1e64ea7c
Ryan Kotzen <ryan@encentivize.co.za>
2016-09-18 21:31:46 +0200

    adding a file to test calls.

---
+++ test/api/krpc/manually-specified.integration.js
@@ -0,0 +1,42 @@
+'use strict';
+require('../../init');
+let Client = require('../../../lib/client');
+
+describe('Get-status', function () {
+    it('Should work', function (done) {
+        let client = Client();
+        client.on('open', onOpen(client));
+        client.on('error', onError(done));
+        client.on('message', onMessage(done));
+    });
+});
+
+let callStack = [];
+function onOpen(client) {
+    return function () {
+        let procedure = client.services.krpc.getStatus();
+        callStack.push(procedure.decode);
+        client.send(procedure.call);
+    };
+}
+
+function onError(done) {
+    return function (err) {
+        done(err);
+    };
+}
+
+function onMessage(done) {
+    return function (response) {
+        expect(response.error).to.not.be.ok();
+        expect(response.results.length).to.equal(1);
+        response.results.forEach(function (result) {
+            expect(result.error).to.not.be.ok();
+            let decodeType = callStack.pop();
+            let decodedResult = Client.decode(result.value, decodeType);
+            expect(decodedResult).to.be.ok();
+        });
+        return done();
+    };
+}
+
