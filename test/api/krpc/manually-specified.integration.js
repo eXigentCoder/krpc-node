4545f529106f345e9a8a9889ea621060b8827166
Ryan Kotzen <ryan@encentivize.co.za>
2016-09-21 18:20:44 +0200

    trying to get the code to call abort.

--- test/api/krpc/manually-specified.integration.js
+++ test/api/krpc/manually-specified.integration.js
@@ -7,14 +7,17 @@ describe('Get-status', function () {
         let client = Client();
         client.on('open', onOpen(client));
         client.on('error', onError(done));
-        client.on('message', onMessage(done));
+        client.on('message', onMessage(client, done));
     });
 });
 
 let callStack = [];
 function onOpen(client) {
     return function () {
-        let procedure = client.services.spaceCenter.clearTarget();
+        // let procedure = client.services.spaceCenter.clearTarget();
+        // callStack.push(procedure.decode);
+        // client.send(procedure.call);
+        let procedure = client.services.spaceCenter.getActiveVessel();
         callStack.push(procedure.decode);
         client.send(procedure.call);
     };
@@ -25,9 +28,12 @@ function onError(done) {
         done(err);
     };
 }
-
-function onMessage(done) {
+var counter = 0;
+var control = null;
+var vessel = null;
+function onMessage(client, done) {
     return function (response) {
+        counter++;
         expect(response.error).to.not.be.ok();
         expect(response.results.length).to.equal(1);
         response.results.forEach(function (result) {
@@ -36,9 +42,22 @@ function onMessage(done) {
             if (decodeType) {
                 let decodedResult = Client.decode(result.value, decodeType);
                 expect(decodedResult).to.be.ok();
+                if (counter === 1) {
+                    vessel = decodedResult;
+                    let procedure = client.services.spaceCenter.vesselGetControl(vessel.id);
+                    callStack.push(procedure.decode);
+                    client.send(procedure.call);
+                }
+                else if (counter === 2) {
+                    control = decodedResult;
+                    let procedure = client.services.spaceCenter.controlSetAbort(control.id, true);
+                    callStack.push(procedure.decode);
+                    client.send(procedure.call);
+                } else {
+                    return done();
+                }
             }
         });
-        return done();
     };
 }
 
