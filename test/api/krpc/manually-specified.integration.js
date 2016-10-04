c176d152d930308ad61790728ce6158a586e98ac
Ryan Kotzen <ryan@encentivize.co.za>
2016-10-04 09:29:30 +0200

    trying to get this code to work correctly.

--- test/api/krpc/manually-specified.integration.js
+++ test/api/krpc/manually-specified.integration.js
@@ -5,6 +5,8 @@ let client;
 var success = false;
 let util = require('util');
 var proto = require('../../../lib/utilities/proto');
+let ProtoBuf = require('protobufjs');
+var abUtil = require('../../../lib/utilities/array-buffer');
 
 describe('Get-status', function () {
     it('Should work', function (done) {
@@ -21,7 +23,7 @@ function onOpen() {
     // let procedure = client.services.spaceCenter.clearTarget();
     // callStack.push(procedure.decode);
     // client.send(procedure.call);
-    let procedure = client.services.spaceCenter.getActiveVessel();
+    let procedure = client.services.spaceCenter.getVessels();
     callStack.push(procedure.decode);
     client.send(procedure.call);
 };
@@ -51,19 +53,31 @@ function onMessage(done) {
             expect(result.error).to.not.be.ok();
             let decodeType = callStack.pop();
             if (decodeType) {
-                let decodedResult = Client.decode(result.value, decodeType);
-                expect(decodedResult).to.be.ok();
+                //let decodedResult = Client.decode(result.value, decodeType);
+                //expect(decodedResult).to.be.ok();
                 if (counter === 1) {
-                    vessel = decodedResult;
-                    let arg = new proto.krpc.schema.Argument(0, result.value);
-                    let call = new proto.krpc.schema.ProcedureCall('SpaceCenter', 'Vessel_get_Control', arg);
-                    //let procedure = client.services.spaceCenter.vesselGetControl(result.value);
-                    //callStack.push(procedure.decode);
-                    client.send(call);
+                    //vessel = decodedResult;
+                    // var arrayB = abUtil.arrayBufferToBuffer(result.value.buffer);
+                    // var TStream = proto.builder.lookup("krpc.schema.Stream");
+                    // var id = TStream.children[0];
+                    // var t7 = id.decode(0, result.value);
+                    // var test = proto.krpc.schema.List.decode(result.value);
+                    // //var t3 = result.value.readUint64();
+                    // var ab = result.value.readUInt8();
+                    // let buffer = new Buffer(ab);
+                    // let venumVal = buffer.readUInt8() / 2;
+                    // let arg = new proto.krpc.schema.Argument(0, result.value.buffer);
+                    // let call = new proto.krpc.schema.ProcedureCall('SpaceCenter', 'Vessel_get_Control', [arg]);
+                    //let procedure = client.services.spaceCenter.vesselGetControl(result.value.buffer);
+                    let payload = Buffer.from([0x01]);
+                    let procedure = client.services.spaceCenter.vesselGetControl(payload);
+                    callStack.push(procedure.decode);
+                    client.send(procedure.call);
                 }
                 else if (counter === 2) {
-                    control = decodedResult;
-                    let procedure = client.services.spaceCenter.controlSetAbort(control.id, true);
+                    //control = decodedResult;
+                    let payload = Buffer.from([0x06]);
+                    let procedure = client.services.spaceCenter.controlSetAbort(payload, true);
                     callStack.push(procedure.decode);
                     client.send(procedure.call);
                 } else {
@@ -74,4 +88,3 @@ function onMessage(done) {
         });
     };
 }
-
