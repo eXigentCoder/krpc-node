5aa6279e903734ebaa3240ad2fe2a05fda7dfbe1
Ryan Kotzen <ryan@encentivize.co.za>
2017-03-12 14:24:44 +0200

    updating test to work with callback on send.

--- test/api/krpc/get-clients.integration.js
+++ test/api/krpc/get-clients.integration.js
@@ -1,45 +1,51 @@
 'use strict';
 require('../../init');
-let Client = require('../../../lib/client');
+let createClient = require('../../../lib/client');
+const async = require('async');
+
 describe('Get-clients', function () {
     it('Should work', function (done) {
-        Client(null, clientCreated);
-
-        function clientCreated(err, client) {
+        async.waterfall([
+            async.apply(createClient, null),
+            clientCreated
+        ], function (err) {
             if (err) {
                 return done(err);
             }
-            client.rpc.on('open', onOpen(client));
+        });
+
+        function clientCreated(client, callback) {
+            client.rpc.on('open', onOpen(client, done));
             client.rpc.on('error', onError(done));
-            //client.rpc.on('message', onMessage(done));
+            callback();
         }
     });
 });
 
-function onOpen(client) {
+
+function onOpen(client, done) {
     return function () {
-        client.send(client.services.krpc.getClients(), function (err, response) {
-            console.log(err, response);
-        });
+        client.send(client.services.krpc.getClients(), getClientsComplete);
+
+        function getClientsComplete(err, response) {
+            if (err) {
+                return done(err);
+            }
+            expect(response.error).to.not.be.ok();
+            expect(response.results.length).to.equal(1);
+            let result = response.results[0];
+            expect(result.error).to.not.be.ok();
+            result.value.items.forEach(function (item) {
+                expect(item).to.be.ok();
+                //todo
+            });
+            return done();
+        }
     };
 }
 
 function onError(done) {
     return function (err) {
         done(err);
-    };
-}
-
-function onMessage(done) {
-    return function (response) {
-        expect(response.error).to.not.be.ok();
-        expect(response.results.length).to.equal(1);
-        let result = response.results[0];
-        expect(result.error).to.not.be.ok();
-        result.value.items.forEach(function (item) {
-            expect(item).to.be.ok();
-            //todo
-        });
-        return done();
     };
 }
