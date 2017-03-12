c48f8c0ac00b9d5ba84887cd771c3ae56ae9881f
Ryan Kotzen <ryan@encentivize.co.za>
2017-03-12 18:35:51 +0200

    string decode + tests.

---
+++ test/api/decoder-type-tests/string.js
@@ -0,0 +1,52 @@
+'use strict';
+require('../../init');
+let Client = require('../../../lib/client');
+const async = require('async');
+
+describe('Decoder type tests', function () {
+    it('Should be able to decode a `uInt64` and `double`', function (done) {
+        Client(null, clientCreated);
+
+        function clientCreated(err, client) {
+            if (err) {
+                return done(err);
+            }
+            let data = {client: client};
+            async.waterfall([
+                async.apply(getVessel, data),
+                getBiome,
+                getVesselName
+            ], done);
+        }
+    });
+});
+
+function getVessel(data, callback) {
+    data.client.send(data.client.services.spaceCenter.getActiveVessel(), function (err, response) {
+        if (err) {
+            return callback(err);
+        }
+        data.vesselId = response.results[0].value;
+        return callback(null, data);
+    });
+}
+
+function getBiome(data, callback) {
+    data.client.send(data.client.services.spaceCenter.vesselGetBiome(data.vesselId), function (err, response) {
+        if (err) {
+            return callback(err);
+        }
+        data.biome = response.results[0].value;
+        return callback(null, data);
+    });
+}
+
+function getVesselName(data, callback) {
+    data.client.send(data.client.services.spaceCenter.vesselGetName(data.vesselId), function (err, response) {
+        if (err) {
+            return callback(err);
+        }
+        data.vesselName = response.results[0].value;
+        return callback(null, data);
+    });
+}
