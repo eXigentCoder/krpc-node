5d98085d44bb01a98665f4d7ec487b5d3e7b5f53
Ryan Kotzen <ryan@encentivize.co.za>
2017-03-12 19:07:54 +0200

    refactoring ui tests

--- test/api/ui/message.js
+++ test/api/ui/message.js
@@ -1,39 +1,32 @@
 'use strict';
 require('../../init');
 let Client = require('../../../lib/client');
-describe('Get-clients', function () {
-    it('Should work', function (done) {
+describe('UI', function () {
+    it('Display message', function (done) {
         Client(null, clientCreated);
 
-        function clientCreated(err, client) {
-            if (err) {
-                return done(err);
+        function clientCreated(clientErr, client) {
+            if (clientErr) {
+                return done(clientErr);
             }
-            client.rpc.on('open', onOpen(client));
             client.rpc.on('error', onError(done));
-            client.rpc.on('message', onMessage(done));
+            client.send(client.services.ui.message('Hello World!', 5, 'TopCenter'), function (messegeErr, response) {
+                if (messegeErr) {
+                    return done(messegeErr);
+                }
+                expect(response.error).to.not.be.ok();
+                expect(response.results.length).to.equal(1);
+                let clientsResponse = response.results[0];
+                expect(clientsResponse.error).to.not.be.ok();
+                return done();
+            });
         }
     });
 });
 
-function onOpen(client) {
-    return function () {
-        client.send(client.services.ui.message('Hello World!', 5, 'TopCenter'));
-    };
-}
 
 function onError(done) {
     return function (err) {
         done(err);
-    };
-}
-
-function onMessage(done) {
-    return function (response) {
-        expect(response.error).to.not.be.ok();
-        expect(response.results.length).to.equal(1);
-        let clientsResponse = response.results[0];
-        expect(clientsResponse.error).to.not.be.ok();
-        return done();
     };
 }
