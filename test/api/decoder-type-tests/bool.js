6ec8e156ee99578aedd04b262fca0e24834daf3d
Ryan Kotzen <ryan@encentivize.co.za>
2017-03-12 18:27:36 +0200

    test + fix for bool

---
+++ test/api/decoder-type-tests/bool.js
@@ -0,0 +1,51 @@
+'use strict';
+require('../../init');
+let Client = require('../../../lib/client');
+const async = require('async');
+
+describe('Decoder type tests', function () {
+    it('Should be able to decode a `uInt64` and `double`', function (done) {
+        Client(null, clientCreated);
+
+        function clientCreated(err, client) {
+            if (err) {
+                return done(err);
+            }
+            let data = {client: client};
+            async.waterfall([
+                async.apply(getVessel, data),
+                getControl,
+                controlGetThrottle
+            ], done);
+        }
+    });
+});
+function getVessel(data, callback) {
+    data.client.send(data.client.services.spaceCenter.getActiveVessel(), function (err, response) {
+        if (err) {
+            return callback(err);
+        }
+        data.vesselId = response.results[0].value;
+        return callback(null, data);
+    });
+}
+
+function getControl(data, callback) {
+    data.client.send(data.client.services.spaceCenter.vesselGetControl(data.vesselId), function (err, response) {
+        if (err) {
+            return callback(err);
+        }
+        data.controlId = response.results[0].value;
+        return callback(null, data);
+    });
+}
+
+function controlGetThrottle(data, callback) {
+    data.client.send(data.client.services.spaceCenter.controlGetBrakes(data.controlId), function (err, response) {
+        if (err) {
+            return callback(err);
+        }
+        data.brakesOn = response.results[0].value;
+        return callback(null, data);
+    });
+}
