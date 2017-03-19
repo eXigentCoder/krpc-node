b70afc1c77b51261b78c01af4616ef5aaff5b5b6
Ryan Kotzen <ryan@encentivize.co.za>
2017-03-19 15:55:59 +0200

    adding failing test for single individual calls.

--- test/api/krpc/multiple-calls.js
+++ test/api/krpc/multiple-calls.js
@@ -23,9 +23,56 @@ describe('Multiple Calls', function () {
             }
             let clientId = getResultN(response, 0).toString('base64');
             expect(clientId).to.be.ok();
+            expect(clientId.length).to.be.greaterThan(5);
             let vesselId = getResultN(response, 1);
             expect(vesselId).to.be.ok();
             return done();
+        }
+    });
+    it('Should work with multiple single calls and callbacks', function (done) {
+        Client(null, clientCreated);
+        let complete = 0;
+        let total = 0;
+
+        function clientCreated(err, client) {
+            if (err) {
+                return done(err);
+            }
+            let getClientId = client.services.krpc.getClientId();
+            let getActiveVessel = client.services.spaceCenter.getActiveVessel();
+            send(client, getClientId, getClientIdComplete);
+            send(client, getActiveVessel, getActiveVesselComplete);
+        }
+
+        function getClientIdComplete(err, response) {
+            if (err) {
+                return done(err);
+            }
+            let clientId = getResultN(response, 0).toString('base64');
+            expect(clientId).to.be.ok();
+            expect(clientId.length).to.be.greaterThan(5);
+            return singleCallComplete();
+        }
+
+        function getActiveVesselComplete(err, response) {
+            if (err) {
+                return done(err);
+            }
+            let vesselId = getResultN(response, 0);
+            expect(vesselId).to.be.ok();
+            return singleCallComplete();
+        }
+
+        function send(client, call, callback) {
+            total++;
+            client.send(call, callback);
+        }
+
+        function singleCallComplete() {
+            complete++;
+            if (complete === total) {
+                done();
+            }
         }
     });
 });
