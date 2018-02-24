c1c1177f094867ee3e4c9b25d42f3c655134118f
Ryan Kotzen <potz666@gmail.com>
2018-02-24 20:44:40 +0200

    Updating the .send  function to use async or callback

--- test/api/decoder-tests/bool.js
+++ test/api/decoder-tests/bool.js
@@ -1,22 +1,22 @@
 'use strict';
 require('../../init');
 let createClient = require('../../../lib/client');
-let util = require('util');
 
 describe('Decoding - bool', function() {
     it('Should be able to decode a `bool` successfully', async function() {
         const client = await createClient();
         try {
-            const send = util.promisify(client.send);
-            let response = await send(client.services.spaceCenter.getActiveVessel());
+            let response = await client.send(client.services.spaceCenter.getActiveVessel());
             const vesselId = response.results[0].value;
-            response = await send(client.services.spaceCenter.vesselGetControl(vesselId));
+            response = await client.send(client.services.spaceCenter.vesselGetControl(vesselId));
             const controlId = response.results[0].value;
-            response = await send(client.services.spaceCenter.controlGetBrakes(controlId));
+            response = await client.send(client.services.spaceCenter.controlGetBrakes(controlId));
             const brakesOn = response.results[0].value;
             console.log(`brakesOn : ${brakesOn}`);
-        } finally {
             client.close();
+        } catch (err) {
+            await client.close();
+            throw err;
         }
     });
 });
