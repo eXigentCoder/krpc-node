bc19370fc886c8fa39e4acfeed23d50b8f51e40f
Ryan Kotzen <potz666@gmail.com>
2018-02-24 20:48:51 +0200

    Updating code so we have both versions, CB + Async

---
+++ test/api/decoder-tests/bool.async.js
@@ -0,0 +1,22 @@
+'use strict';
+require('../../init');
+let createClient = require('../../../lib/client');
+
+describe('Decoding - bool', function() {
+    it('Should be able to decode a `bool` successfully', async function() {
+        const client = await createClient();
+        try {
+            let response = await client.send(client.services.spaceCenter.getActiveVessel());
+            const vesselId = response.results[0].value;
+            response = await client.send(client.services.spaceCenter.vesselGetControl(vesselId));
+            const controlId = response.results[0].value;
+            response = await client.send(client.services.spaceCenter.controlGetBrakes(controlId));
+            const brakesOn = response.results[0].value;
+            console.log(`brakesOn : ${brakesOn}`);
+            client.close();
+        } catch (err) {
+            await client.close();
+            throw err;
+        }
+    });
+});
