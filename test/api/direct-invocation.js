a0031edffde951ee8806e031ed8279a4459d67db
Ryan Kotzen <potz666@gmail.com>
2018-03-01 21:57:38 +0200

    Updating code + test to pass in the id of the object.

--- test/api/direct-invocation.js
+++ test/api/direct-invocation.js
@@ -11,8 +11,10 @@ describe('Getting Started - async', function() {
             let response = await client.send(spaceCenter.getActiveVessel());
             let vessel = response.results[0].value;
             response = await spaceCenter._vesselGetControl(vessel.id);
-            let controlId = response.results[0].value;
-            expect(controlId).to.be.ok();
+            let control = response.results[0].value;
+            expect(control).to.be.ok();
+            let control2 = await vessel.control.get();
+            expect(control2).to.be.ok();
         } catch (err) {
             await client.close();
             throw err;
